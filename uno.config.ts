896b770988b695c592d88b5c49b6e4d1fa31cb02
Jason Luke <jasonluke97@gmail.com>
2026-01-21 19:21:15 -0500

    hopefully safelisted icons

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,29 @@
+// import { defineConfig } from 'unocss'
+// import { presetStarlightIcons } from 'starlight-plugin-icons/uno'
+
+// export default defineConfig({
+//   presets: [presetStarlightIcons()],
+// })
+
 import { defineConfig } from 'unocss'
 import { presetStarlightIcons } from 'starlight-plugin-icons/uno'
 
 export default defineConfig({
-  presets: [presetStarlightIcons()],
+  presets: [
+    presetStarlightIcons(),
+  ],
+
+  safelist: [
+    // game-icons
+    'i-game-icons:night-sky',
+    'i-game-icons:herbs-bundle',
+    'i-game-icons:wolf-howl',
+    'i-game-icons:biohazard',
+    'i-game-icons:bird-claw',
+    'i-game-icons:slingshot',
+
+    // weather icons
+    'i-wi:moon-waning-crescent-3',
+    'i-wi:moon-waxing-crescent-4',
+  ],
 })
